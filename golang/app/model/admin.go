91282a82af611f66d1a2312ffade23deccc2d681
okawa <0cu2045d@gmail.com>
2023-12-14 00:36:28 +0900

    型指定が間違えていたので修正

--- golang/app/model/admin.go
+++ golang/app/model/admin.go
@@ -10,8 +10,8 @@ type Admin struct {
 	Id        uint           `json:"id" gorm:"primary_key; autoIncrement"`
 	Name      string         `json:"name" gorm:"not null; comment:名前"`
 	LoginId   string         `json:"login_id" gorm:"unique; not null; comment:ログインID"`
-	Role      bool           `json:"role" gorm:"not null; comment:権限"`
-	Status    int8           `json:"status" gorm:"not null; default:1 ; comment:ステータス(0:無効,1:有効)"`
+	Role      int8           `json:"role" gorm:"not null; comment:権限"`
+	Status    bool           `json:"status" gorm:"not null; default:1 ; comment:ステータス(0:無効,1:有効)"`
 	CreatedAt time.Time      `json:"created_at" gorm:"comment:作成日時"`
 	UpdatedAt time.Time      `json:"updated_at" gorm:"comment:更新日時"`
 	DeletedAt gorm.DeletedAt `json:"-" gorm:"comment:論理削除日時"`
@@ -21,7 +21,7 @@ type AdminResponse struct {
 	Id        uint      `json:"id"`
 	Name      string    `json:"name"`
 	LoginId   string    `json:"login_id"`
-	Role      bool      `json:"role"`
-	Status    int8      `json:"status"`
+	Role      int8      `json:"role"`
+	Status    bool      `json:"status"`
 	CreatedAt time.Time `json:"created_at"`
 }
